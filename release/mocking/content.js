335e47213084b1621f5c7313c2c6b85e2e1ba3ad
Matthias Buchholz <matthias@quarterland.net>
2017-05-19 01:43:15 +0200

    build

--- release/mocking/content.js
+++ release/mocking/content.js
@@ -5,6 +5,16 @@ var _cuid = require('cuid');
 var parseModifiers = function (value) { return (value.match(/\.(\w+)/gm) || []).map(function (v) { return v.substr(1); }); };
 var parseTypeName = function (value) { return (value.match(/^\w+/) || [])[0]; };
 var parseTypeParams = function (value) { return (value.match(/(\(.+\))/) || [])[0]; };
+function isMockSchema(other) {
+    return ('type' in other
+        &&
+            ('cuid' in other
+                ||
+                    'modifiers' in other
+                ||
+                    'typeParams' in other));
+}
+exports.isMockSchema = isMockSchema;
 var parse = function (value) {
     var typeParams = parseTypeParams(value);
     var typeName = parseTypeName(value);
@@ -30,18 +40,29 @@ exports.mockFragment = function (children, modifiers) {
         modifiers: modifiers,
         type: 'fragment',
         children: children.map(function (child) {
-            if (child.isKioNode)
+            if (child instanceof kio_ng2_1.KioNodeModel) {
                 return child;
+            }
+            if (isMockSchema(child)) {
+                if (child.type !== kio_ng2_1.KioNodeType.fragment) {
+                    return new kio_ng2_1.KioContentModel(child.type, child);
+                }
+                if (child.type === kio_ng2_1.KioNodeType.fragment) {
+                    return new kio_ng2_1.KioFragmentModel(child);
+                }
+            }
             if (Array.isArray(child)) {
                 return exports.mockFragment(child[0], child[1]);
             }
-            return exports.mockContentFromString(child);
+            if ('string' === typeof child) {
+                return exports.mockContentFromString(child);
+            }
         })
     });
 };
 exports.mockContentFromString = function (selector) {
     var _a = selector.match(/^(\w+)(\(.*\))?(\.\w+){0,}/), m = _a[0], typeName = _a[1], typeParams = _a[2], modifiers = _a.slice(3);
-    return exports.mockContent(typeName, modifiers);
+    return mockContent(typeName, modifiers);
 };
 exports.mockPrimitive = function (type, modifiers, cuid, parent) {
     var data = {
@@ -52,7 +73,7 @@ exports.mockPrimitive = function (type, modifiers, cuid, parent) {
     };
     return new kio_ng2_1.KioContentModel(type, data, parent);
 };
-exports.mockContent = function (value, modifiers, parent) {
+function mockContent(value, modifiers, parent) {
     if (modifiers === void 0) { modifiers = []; }
     if ('string' === typeof value) {
         var _a = parse(value), type = _a.type, _b = _a.modifiers, modifiers_1 = _b === void 0 ? [] : _b, _c = _a.typeParams, typeParams = _c === void 0 ? '' : _c;
@@ -67,5 +88,6 @@ exports.mockContent = function (value, modifiers, parent) {
     }
     /*.map ( tupelSrc => tupelSrc.split('=') )
     .map ( ([key,value]) => ({key, value}) )*/
-};
+}
+exports.mockContent = mockContent;
 //# sourceMappingURL=content.js.map
